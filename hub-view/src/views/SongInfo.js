aed344d0aff60680b7e2762fa1d5842170845ac6
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-18 16:34:49 -0700

    fix release date display

--- hub-view/src/views/SongInfo.js
+++ hub-view/src/views/SongInfo.js
@@ -21,7 +21,7 @@ const SongInfo = (props) => {
           <div><h2><a href={props.songInfo.songUrl}>{props.songInfo.title}</a> by</h2></div>
           <div className="songInfo-artists-container">
             {props.songInfo.artists.map((artist, i) => {
-              return  <div key={artist.id}><h2>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? ",":""}</h2></div>
+              return  <h2 key={artist.id}>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? ",":""}</h2>
             })}
           </div>
         </div>
@@ -29,7 +29,7 @@ const SongInfo = (props) => {
         <div>
           <h2>Track {props.songInfo.trackNumber} on <a href={props.songInfo.albumUrl}>{props.songInfo.albumName}</a></h2>
           <h3>{props.songInfo.time}</h3>
-          <h3>Released {props.songInfo.releaseDate}</h3>
+          <h3>Released {props.songInfo.albumReleaseDate}</h3>
         </div>
         <div><h2>ARTIST HEADSHOT</h2></div>
     </div>
