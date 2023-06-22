de174eae84b943edbf162df3dc7599b00c6c297f
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-22 14:46:00 -0700

    Add vinyl effect for each album cover

--- hub-view/src/views/SongInfo.js
+++ hub-view/src/views/SongInfo.js
@@ -7,10 +7,12 @@ const SongInfo = (props) => {
     </div>
   )
 
+  const vinyls = ['./assets/orange-vinyl.png', './assets/red-vinyl.png', './assets/yellow-vinyl.png']
   return (
     <div className="right-body-songInfo-container">
         <div className="right-body-songInfo-image-container">
-          <a href={props.songInfo.albumUrl}><img src={props.songInfo.albumImages[1].url} alt="Song Cover"/></a>
+          <div className="right-body-songInfo-image"><a href={props.songInfo.albumUrl}><img src={props.songInfo.albumImages[1].url} alt="Song Cover"/></a></div>
+          <div className="right-body-songInfo-vinyl"><img src={require(`${vinyls[Math.floor(Math.random(3)*3)]}`)} alt="vinyl"/></div>
         </div>
 
         <div className="right-body-songInfo-info-container">
