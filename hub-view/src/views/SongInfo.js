6c080a4f9822057cf4a9b4e7102d575393e35411
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-21 17:37:12 -0700

    Style song info box

--- hub-view/src/views/SongInfo.js
+++ hub-view/src/views/SongInfo.js
@@ -8,26 +8,23 @@ const SongInfo = (props) => {
   )
 
   return (
-    <div>
-        <div>
+    <div className="right-body-songInfo-container">
+        <div className="right-body-songInfo-image-container">
           <a href={props.songInfo.albumUrl}><img src={props.songInfo.albumImages[1].url} alt="Song Cover"/></a>
         </div>
 
-        <div className="songInfo-song-title-container">
-          <div><h2><a href={props.songInfo.songUrl}>{props.songInfo.title}</a> by</h2></div>
-          <div className="songInfo-artists-container">
+        <div className="right-body-songInfo-info-container">
+          <p><a href={props.songInfo.songUrl}>{props.songInfo.title}</a></p>
+          <div className="right-body-songInfo-info-title">
+            <p>by</p>
             {props.songInfo.artists.map((artist, i) => {
-              return  <h2 key={artist.id}>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? ",":""}</h2>
+              return  <p key={artist.id}>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? ",":""}</p>
             })}
           </div>
+          <p>Track {props.songInfo.trackNumber} on <a href={props.songInfo.albumUrl}>{props.songInfo.albumName}</a></p>
+          <p>Released on {props.songInfo.albumReleaseDate}</p>
+          <p>{props.songInfo.time}</p>      
         </div>
-
-        <div>
-          <h2>Track {props.songInfo.trackNumber} on <a href={props.songInfo.albumUrl}>{props.songInfo.albumName}</a></h2>
-          <h3>{props.songInfo.time}</h3>
-          <h3>Released {props.songInfo.albumReleaseDate}</h3>
-        </div>
-        <div><h2>ARTIST HEADSHOT</h2></div>
     </div>
   );
 }
