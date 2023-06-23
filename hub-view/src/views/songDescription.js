dae2cfceb060039b89db7d19dd7a8a5684884c5d
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-23 14:52:51 -0700

    Change Song details styling

--- hub-view/src/views/songDescription.js
+++ hub-view/src/views/songDescription.js
@@ -4,16 +4,23 @@ import React from "react"
 export default function SongDescription(props) {
   return (
     <div className="songDescription-container">
-        <p><a href={props.songInfo.songUrl}>{props.songInfo.title}</a></p>
-        <div className="songDescription-title">
-        <p>by</p>
-        {props.songInfo.artists.map((artist, i) => {
-            return  <p key={artist.id}>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? ",":""}</p>
-        })}
+        <p className="songDescription-title"><a href={props.songInfo.songUrl}>{props.songInfo.title}</a></p>
+        <div className="songDescription-artists">
+          {props.songInfo.artists.map((artist, i) => {
+              return  <p key={artist.id}>&nbsp;<a  href={artist.url}>{artist.name}</a>{i !== props.songInfo.artists.length-1? `, `:""}</p>
+          })}
         </div>
-        <p>Track {props.songInfo.trackNumber} on <a href={props.songInfo.albumUrl}>{props.songInfo.albumName}</a></p>
-        <p>Released on {props.songInfo.albumReleaseDate}</p>
-        <p>{props.songInfo.time}</p>
+
+        <div className="songDescription-info">
+          <p>
+            Track {props.songInfo.trackNumber} on <a href={props.songInfo.albumUrl}>{props.songInfo.albumName}</a>
+            <br />
+            Released on {props.songInfo.albumReleaseDate}
+            <br />
+            {props.songInfo.time}
+          </p>
+        </div>
+        
     </div>
   );
 }
