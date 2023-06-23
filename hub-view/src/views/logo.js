9139bdc1ccd27fb51e47b1a8d36f5c66385e7988
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-22 17:49:57 -0700

    update website name

--- hub-view/src/views/logo.js
+++ hub-view/src/views/logo.js
@@ -3,7 +3,7 @@ import React from "react"
 const Logo = () => {
   return (
     <div className="topBar-container-header">
-        <p className="topBar-container-header-title">Music Hub</p>
+        <p className="topBar-container-header-title">Song search</p>
         <p className="topBar-container-header-subtitle">by <a href="https://adrianslopez.github.io/">Adrian Salgado Lopez</a></p>
     </div>
   );
