d2fcb7197c553b29025278debe9b143f002cd522
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-26 15:04:23 -0700

    update states

--- hub-view/src/views/body.js
+++ hub-view/src/views/body.js
@@ -6,7 +6,7 @@ import Recommendations from "./recommendations";
 export default function Body(props) {
   return (
       <div className="body">
-        <Recommendations publicRecommendations={props.publicRecommendations}/>
+        <Recommendations publicRecommendations={props.publicRecommendations} sendChosenSongId={props.sendChosenSongId} sendUserSearchTerm={props.sendUserSearchTerm} sendUrl={props.sendUrl}/>
         {props.songResults.length === 0? <SearchResults />: <SearchResults results={props.songResults} sendChosenSongId={props.sendChosenSongId}  chosenSongId={props.chosenSongId} userSearchTerm={props.userSearchTerm} sendUrl={props.sendUrl}/>}  
         {props.songInfo.length === 0? <SongInfo />: <SongInfo chosenSongId={props.chosenSongId} userSearchTerm={props.userSearchTerm} songInfo={props.songInfo} updatePublicRec={props.updatePublicRec}/>}
       </div> 
--- hub-view/src/views/recommendations.js
+++ hub-view/src/views/recommendations.js
@@ -3,10 +3,20 @@ import React from "react"
 export default function Recommendations(props) {
     if(props.publicRecommendations === undefined) return 
 
+    const onRecClick = (e) => {
+        e.preventDefault()
+
+        const songId = e.target.getAttribute("data-index-songid")
+        const term = e.target.getAttribute("data-index-term");
+        props.sendChosenSongId(songId)
+        props.sendUserSearchTerm(term)
+        props.sendUrl(`/search/?song=${term}`)
+    }
+
     const content = props.publicRecommendations.map((rec, i) => {
         return (
             <div className="recommendations-item-container" key={rec._id}>
-                <div className={i===0? "recommendations-item-img-first":"recommendations-item-img"}><img  data-index-song={i} src={rec.albumImages[0].url} alt="album"/></div>
+                <div onClick={onRecClick} data-index-songid={rec.songId} data-index-term={rec.userSearchTerm} className={i===0? "recommendations-item-img-first":"recommendations-item-img"}><img  data-index-song={i} src={rec.albumImages[0].url} alt="album"/></div>
                 <div className={i===0? "recommendations-item-description-first":"recommendations-item-description"}>Recommended by <br /> {rec.userName}</div>
             </div>
             
