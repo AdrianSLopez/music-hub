9aab1a091281d951c7c2269f5c561a50dbaec7c7
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-25 17:36:21 -0700

    Update sidebar recommendation title

--- hub-view/src/views/recommendations.js
+++ hub-view/src/views/recommendations.js
@@ -22,7 +22,7 @@ export default function Recommendations(props) {
     return (   
         <div className="recommendations-section">
             <div>
-                <div className="recommendations-title">Recommendations</div>
+                <div className="recommendations-title">Public <br/>Recommendations</div>
                 <div className="recommendations">
                     {content}
                 </div>    
