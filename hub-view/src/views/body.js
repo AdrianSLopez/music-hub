1e268565415c6bba6968390face36e767601fed6
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-25 17:01:12 -0700

    Add and style recommendation sidebar

--- hub-view/src/views/body.js
+++ hub-view/src/views/body.js
@@ -1,13 +1,14 @@
 import React from "react"
 import SearchResults from "./searchResults";
 import SongInfo from "./songInfo";
+import Recommendations from "./recommendations";
 
 export default function Body(props) {
-
   return (
       <div className="body">
-        {props.songResults.length === 0? <SearchResults />: <SearchResults results={props.songResults} sendChosenSongId={props.sendChosenSongId}  chosenSongId={props.chosenSongId} userSearchTerm={props.userSearchTerm} sendUrl={props.sendUrl}/>}
-        {props.songInfo.length === 0? <SongInfo />: <SongInfo songInfo={props.songInfo}/>}
+        <Recommendations publicRecommendations={props.publicRecommendations}/>
+        {props.songResults.length === 0? <SearchResults />: <SearchResults results={props.songResults} sendChosenSongId={props.sendChosenSongId}  chosenSongId={props.chosenSongId} userSearchTerm={props.userSearchTerm} sendUrl={props.sendUrl}/>}  
+        {props.songInfo.length === 0? <SongInfo />: <SongInfo chosenSongId={props.chosenSongId} userSearchTerm={props.userSearchTerm} songInfo={props.songInfo} updatePublicRec={props.updatePublicRec}/>}
       </div> 
   );
 }
---
+++ hub-view/src/views/recommendations.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+export default function Recommendations(props) {
+    if(props.publicRecommendations === undefined) return 
+
+    // const onRecClick = (e) => {
+    //     e.preventDefault()
+    //     const index = e.target.getAttribute("data-index-song")
+    //     console.log("recommendation clicked on: [" + props.publicRecommendations[index].title + "]" )
+    // }
+
+    const content = props.publicRecommendations.map((rec, i) => {
+        return (
+            <div className="recommendations-item-container">
+                <img className={i===0? "recommendations-item-img-first":"recommendations-item-img"} data-index-song={i} src={rec.albumImages[0].url} alt="album"/>
+                <div className={i===0? "recommendations-item-description-first":"recommendations-item-description"}>Recommended by <br /> {rec.userName}</div>
+            </div>
+            
+        )
+    })
+
+    return (   
+        <div className="recommendations-section">
+            <div>
+                <div className="recommendations-title">Recommendations</div>
+                <div className="recommendations">
+                    {content}
+                </div>    
+            </div>
+        </div>
+    );
+}
