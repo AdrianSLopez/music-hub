4f934777ba5330b91b0f84844b19e3e8cf2a652e
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-26 11:08:26 -0700

    update recommend input style

--- hub-view/src/views/recommend.js
+++ hub-view/src/views/recommend.js
@@ -1,44 +1,50 @@
-import React from "react"
+import React, {useState, useRef} from "react"
 
 export default function Recommend(props) {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-    
-        const form = e.target;
-        const formData = new FormData(form);
-        const displayName = formData.get("display-name");
+  const [formName, setFormName] = useState("");
+  const hrtBtn = useRef();
 
-        if(displayName==='') return
+  const handleSubmit = (e) => {
+      e.preventDefault();
 
-        fetch('/publicRecommendations/add', {
-          method: "POST",
-          body: JSON.stringify({
-            _id: props.chosenSongId,
-            userSearchTerm: props.userSearchTerm,
-            title: props.songInfo.title,
-            albumImages: props.songInfo.albumImages,
-            userName: displayName
-          }),
-          headers: {
-            "Content-type": "application/json; charset=UTF-8"
-          }
-        })
-        .then(response => {
-            return
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    
-        props.updatePublicRec(true)
+      const form = e.target;
+      const formData = new FormData(form);
+      const displayName = formData.get("display-name");
+
+      if(displayName==='') {
+        form[0].focus()
+        return
       }
 
-    return (   
-        <div>
-            <form method="post" onSubmit={handleSubmit}>
-                <input type="text" name="display-name" placeholder="Name..."/>
-                <button type="submit" className="fa fa-heart"></button>
-            </form>
-        </div>
-    );
+      fetch('/publicRecommendations/add', {
+        method: "POST",
+        body: JSON.stringify({
+          _id: props.chosenSongId,
+          userSearchTerm: props.userSearchTerm,
+          title: props.songInfo.title,
+          albumImages: props.songInfo.albumImages,
+          userName: displayName
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  
+      props.updatePublicRec(true)
+      setFormName("")
+      form[1].focus()
+    }
+
+  return (   
+      <div className="recommend-container">
+          <form method="post" onSubmit={handleSubmit}>
+              <label>Recommended by: </label>
+              <input type="text" name="display-name" placeholder="Name..." value={formName} onChange={(e)=>{setFormName(e.target.value)}} className="recommend-input"/>
+              <button className="rcmnd-btn" ref={hrtBtn}><i type="submit" className="fa fa-heart"/> </button>
+          </form>
+      </div>
+  );
 }
