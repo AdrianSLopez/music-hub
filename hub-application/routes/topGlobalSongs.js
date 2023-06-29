9fa2ef01160274852f8cf174c7fda0a105756c46
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-28 18:12:32 -0700

    update endpoint to accept offset and handle offset response

--- hub-application/routes/topGlobalSongs.js
+++ hub-application/routes/topGlobalSongs.js
@@ -15,12 +15,19 @@ const _filterTracks = (tracks) => {
     });
 }
 
+const _getOffset = (url) => {
+    if(url === null) return null;
+    
+    return url.split('?')[1].split('&')[0].split('=')[1];
+}
+
 router.get('/', async (req, res) => {
     try {
-        const topGlobalSongs = await api.getTopGlobalSongs();
-        const filtered = _filterTracks(topGlobalSongs)
+        const { offset=0 } = req.query;
+        const topGlobalSongs = await api.getTopGlobalSongs(offset);
+        const filtered = _filterTracks(topGlobalSongs.tracks)
 
-        res.json(filtered)
+        res.json({next: _getOffset(topGlobalSongs.next), previous: _getOffset(topGlobalSongs.previous),  tracks: filtered})
     } catch(error) {
         res.status(500).json(error.toString());
     }
