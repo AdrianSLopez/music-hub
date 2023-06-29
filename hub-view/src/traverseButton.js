1dad7e63e64a39cf42952cff8b1648535006fa0c
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-28 17:27:41 -0700

    temp no next page on top global songs

--- hub-view/src/traverseButton.js
+++ hub-view/src/traverseButton.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default function TraverseButton(props) {
     const sendNewUrl = () => {
-        if(props.offset ==null) return
+        if(props.offset ==null || props.userInput.toLowerCase().split(' ').join('') === 'topglobalsongs') return
+        
         props.sendUrl(`/search/?song=${props.userInput}&offset=${props.offset}`)
         props.sendChosenSongId(0)
     }
