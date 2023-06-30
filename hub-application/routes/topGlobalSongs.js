f21b0598483b6aca93649f50f065d5541bfe4597
Nep2un <57457579+AdrianSLopez@users.noreply.github.com>
2023-06-30 12:27:13 -0700

    remove dead code

--- hub-application/routes/topGlobalSongs.js
+++ hub-application/routes/topGlobalSongs.js
@@ -3,11 +3,6 @@ const api = require('spotify-api');
 
 const _filterTracks = (tracks) => {
     return tracks.map((track) => {
-        const artists = track.artists.map((artist) => {
-            return {id: artist.id, name: artist.name, url: artist.external_urls.spotify, };
-        })
-        const time = `${((track.duration_ms)/60000).toFixed(2)} minutes`;
-
         return {
             id: track.id,
             title: track.name
